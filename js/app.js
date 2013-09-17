acb59ecc41ceed127012c2f9bcce2a7464046f07
pioneersolutions <kaushank@sheridancollege.ca>
2013-09-17 14:05:52 -0400

    Some More Updates

--- js/app.js
+++ js/app.js
@@ -10,7 +10,11 @@ App.Router.map(function() {
 			path : ':user_id'
 		});
 	});
-	this.resource('threed');
+	this.resource('threed', function() {
+		this.resource('threeduser', {
+			path : ':user_id'
+		});
+	});
 
 });
 
@@ -18,7 +22,7 @@ App.Router.map(function() {
 App.Model = Ember.Object.extend({
 
 });
-
+//Create Model for 2D Pipeline
 App.Twod = App.Model.extend({
 	id : null,
 	firstname : null,
@@ -32,10 +36,60 @@ App.Twod = App.Model.extend({
 	designation : null
 });
 
-App.TwodController = Ember.ObjectController.extend({
+//Create Model for 3D Pipeline
+App.Threed = App.Model.extend({
+	id : null,
+	firstname : null,
+	lastname : null,
+	address : null,
+	state : null,
+	country : null,
+	email : null,
+	phone : null,
+	experience : null,
+	designation : null
+});
+
+//Controller for 3D Pipeline
+App.ThreedController = Ember.ObjectController.extend({});
+//3D
+
+App.TwodController = Ember.ObjectController.extend({//2D
 
 });
 
+//Controller for 3D Route
+App.ThreedController = Ember.ArrayController.extend({
+	filteredContent : Ember.computed.oneWay("content"),
+	selectedDesignation : null,
+	designations : [{
+		designation : "Design",
+		id : 1
+	}, {
+		designation : "Writer",
+		id : 2
+	}],
+	actions : {
+		filter : function() {
+			var designation = this.get('selectedDesignation.designation');
+			var filtered = this.get('content').filterProperty('designation', designation);
+			this.set("filteredContent", filtered);
+		},
+		refresh : function() {
+			var refresh = this.get('content');
+			this.set("filteredContent", refresh);
+		}
+	},
+	filteredContent : function() {
+		var searchText = this.get('searchText'), regex = new RegExp(searchText, 'i');
+
+		return this.get('model').filter(function(item) {
+			return regex.test(item.firstname);
+		});
+	}.property('searchText', 'model')
+});
+
+//Controller for 2D Route
 App.TwodController = Ember.ArrayController.extend({
 	filteredContent : Ember.computed.oneWay("content"),
 	selectedDesignation : null,
@@ -51,10 +105,13 @@ App.TwodController = Ember.ArrayController.extend({
 			var designation = this.get('selectedDesignation.designation');
 			var filtered = this.get('content').filterProperty('designation', designation);
 			this.set("filteredContent", filtered);
-
 			// var filtered = this.get('content').filterProperty('designation', "Writer");
 			// this.set("filteredContent", filtered);
 
+		},
+		refresh : function() {
+			var refresh = this.get('content');
+			this.set("filteredContent", refresh);
 		}
 	},
 	filteredContent : function() {
@@ -66,12 +123,21 @@ App.TwodController = Ember.ArrayController.extend({
 	}.property('searchText', 'model')
 });
 
+//2D User Route
 App.TwoduserRoute = Ember.Route.extend({
 	model : function(params) {
 		return App.Twod.findBy(params.user_id);
 	}
 });
 
+//3D User Route
+App.ThreeduserRoute = Ember.Route.extend({
+	model : function(params) {
+		return App.Threed.findBy(params.user_id);
+	}
+});
+
+//Model Functions
 App.Twod.reopenClass({
 	findAll : function() {
 
@@ -96,8 +162,42 @@ App.Twod.reopenClass({
 	}
 });
 
+//Get 3D Users
+App.Threed.reopenClass({
+	findAll : function() {
+
+		return new Ember.RSVP.Promise(function(resolve, reject) {
+			$.getJSON("http://pioneerdev.us/users/index", function(data) {
+				var result = data.users.map(function(row) {
+					return App.Threed.create(row);
+				});
+				resolve(result);
+			}).fail(reject);
+		});
+	},
+	findBy : function(user_id) {
+
+		return new Ember.RSVP.Promise(function(resolve, reject) {
+			var user = App.Threed.create();
+			$.getJSON("http://ankur.local/users/byId/" + user_id, function(data) {
+				var result = user.setProperties(data.user);
+				resolve(result);
+			}).fail(reject);
+		});
+	}
+});
+
+//Which model to back the 2D route?
 App.TwodRoute = Ember.Route.extend({
 	model : function() {
 		return App.Twod.findAll();
 	}
 });
+
+//Which Model to back the 3D route?
+App.ThreedRoute = Ember.Route.extend({
+	model : function() {
+		return App.Threed.findAll();
+	}
+});
+
